955b8b70fc94b383b576fba3818efb273f2a5292
LuanDantas <kidinoop@hotmail.com>
2018-09-02 11:20:09 -0300

    Add project files and initial project configs.

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// Default imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Components imports
+import App from './main/app'
+
+// Render
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/main/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Componente funcional (baseado em uma função)
+export default props => (
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
