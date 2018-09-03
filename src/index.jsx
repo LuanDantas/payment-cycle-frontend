363ea92a8b9c91f88bac54185e19c40d726bd240
LuanDantas <kidinoop@hotmail.com>
2018-09-03 20:08:38 -0300

    Initiate Redux integration with the project

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 // Default imports
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 // Components imports
 import App from './main/app'
+import reducers from './main/reducers'
+
+// Definindo a store para o Redux
+const store = createStore( reducers )
 
 // Render
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+    // Envolver toda a aplicação com a tag Provider do Redux
+    <Provider store={ store }>
+        <App />
+    </Provider>
+, document.getElementById('app'))
---
+++ src/main/reducers.js
@@ -0,0 +1,8 @@
+// Default imports
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    dashboard: () => ({ summary: { credit: 100, debit: 50 } })
+})
+
+export default rootReducer
